6eb1133da0438edef0ffd89d482e8c231c2f8863
ErfanKhebrati <khebrati@gmail.com>
2023-07-16 12:40:25 +0330

    cleaned stuff

---
+++ UI/Program.MessageDisplayers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    partial class Program
+    {
+        static void DisplayError(string message)
+        {
+            ConsoleColor previous = ForegroundColor;
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine(message);
+            ForegroundColor = previous;
+        }
+        static void DisplayInfo(string message)
+        {
+            ConsoleColor previous = ForegroundColor;
+            ForegroundColor = ConsoleColor.Blue;
+            WriteLine(message);
+            ForegroundColor = previous;
+        }
+        static void DisplaySuccess(string message)
+        {
+            ConsoleColor previous = ForegroundColor;
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine(message);
+            ForegroundColor = previous;
+        }
+    }
+}
